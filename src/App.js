5cb3767ebfb6c6d7ec68c5b814198c5f871906ed
Bruno Razera <brunocrazera@gmail.com>
2022-01-20 23:21:30 -0300

    feat: adding the App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import { GiphySearch } from './components/GiphySeach'
+import { Box, ThemeProvider } from '@mui/system'
 
-function App() {
+export default function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ThemeProvider theme={{
+      palette: {
+        primary: {
+          main: '#007FFF',
+          dark: '#0059B2'
+        }
+      }
+    }}>
+      <div className="App">
+        <Box sx={{ display: 'grid' }}>
+          <GiphySearch headerText='test'></GiphySearch>
+        </Box>
+      </div>
+    </ThemeProvider>
+  )
 }
-
-export default App;
